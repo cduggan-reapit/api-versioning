bcb67eb9dbf499cd32429310c651bbe56df7047e
cduggan <cduggan@reapit.com>
2024-01-08 13:41:10 +0000

    chore: add empty project

---
+++ src/Corvid/Corvid.Api/Program.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Asp.Versioning.Conventions;
+using Microsoft.OpenApi.Models;
+
+namespace Corvid.Api;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddControllers();
+        
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        
+        app.MapControllers();
+
+        app.Run();
+    }
+}
